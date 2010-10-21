3624a734fe2da362a960186ee283a74e459700f5
Melvar Chen <Melvar.Chen@googlemail.com>
2010-10-22 01:10:20 +0200

    Replacing the chkprefix binary with a script
    Doesn’t work yet.

--- chkcollision.sh
+++ chkcollision.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sed -re '/^[[:space:]]*#/d' -e '/^$/d' -e 's/^([^:]*>)[[:space:]]*:.*/\1/' xcompose | sort >sequences
-chkprefix sequences
+sed -re '/^[[:space:]]*#/d' -e '/^$/d' -e 's/^([^:]*>)[[:space:]]*:.*/\1/' \
+	xcompose | sort | php -B '$lastline = "�";' -R 'if (strpos($argn, $lastline) === 0) { echo $lastline; echo $argn; }'
+#chkprefix sequences
 
